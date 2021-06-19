7062a6cb659a62f2a1e73e8148ef63daae6705cd
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-06-19 19:52:40 +0200

    Refactor reps state to parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App = () => {
+const App = (): JSX.Element => {
+  const [ setReps, setSetReps ] = useState([2, 5, 9]);
+  
+  const submit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    console.table(setReps);
+  };
+
+  const updateSetReps = (reps: number, index: number) => {
+    const setRepsCopy = [ ...setReps ];
+    setRepsCopy[index] = reps;
+    setSetReps(setRepsCopy);
+  };
+
   return (
     <main>
       <form>
-        <Slider id={1} maxReps={15} unit="Reps" />
-        <Slider id={2} maxReps={15} unit="Reps"  />
-        <Slider id={3} maxReps={15} unit="Seconds" />
+        {setReps.map((reps, index) => {
+          return (
+            <Slider
+              key={index}
+              id={index}
+              reps={reps}
+              updateSetReps={updateSetReps}
+              maxReps={15}
+              unit="Reps"
+            />
+          );
+        })}
+        <button onClick={submit}>Done</button>
       </form>
     </main>
   );
 };
 
-const Slider: React.FC<SliderProps> = ({ id, maxReps, unit }): JSX.Element => {
-  const [ reps, setReps ] = useState(0);
-
+const Slider: React.FC<SliderProps> = ({ id, reps, maxReps, unit, updateSetReps }): JSX.Element => {
   return (
     <div>
-      <label htmlFor={`set-${id}-slider`}>Set {id}</label>
+      <label htmlFor={`set-${id}-slider`}>Set {id + 1}</label>
       <input
         type="range"
         id={`set-${id}-slider`}
@@ -26,7 +48,7 @@ const Slider: React.FC<SliderProps> = ({ id, maxReps, unit }): JSX.Element => {
         min="0"
         max={maxReps}
         value={reps}
-        onChange={e => setReps(Number.parseInt(e.target.value))} />
+        onChange={e => updateSetReps(Number.parseInt(e.target.value), id)} />
       {reps} {unit}
     </div>
   );
@@ -34,8 +56,10 @@ const Slider: React.FC<SliderProps> = ({ id, maxReps, unit }): JSX.Element => {
 
 type SliderProps = {
   id: number,
+  reps: number,
   maxReps: number,
-  unit: Unit
+  unit: Unit,
+  updateSetReps: (reps: number, index: number) => void
 };
 
 type Unit = "Reps" | "Seconds";
