df7e9d8cb299a740c851fecfd04b7d8873c4f9de
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-06-19 21:32:32 +0200

    Add models

--- src/Types.ts
+++ src/Types.ts
@@ -1,1 +1,33 @@
-export type Unit = "Reps" | "Seconds";
+export type Unit = 'Reps' | 'Seconds';
+
+export type Exercise = {
+  id: number;
+  group: string;
+  variant: string;
+  level: number;
+  sets: number;
+  reps: number;
+  unit: string;
+  image: string;
+};
+
+export type WorkoutExercise = {
+  id: string;
+  exerciseId: number;
+  sets: number[];
+};
+
+export type Workout = {
+  id: string;
+  startDate: Date;
+  EndDate: Date;
+  Exercises: WorkoutExercise[];
+};
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  workouts: Workout[];
+};
