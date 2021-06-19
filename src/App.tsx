e1e91496462a351db1fbbb5382d0e816d8dcd307
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-06-19 14:12:03 +0200

    Add basic sliders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [ sets, setSets ] = useState([0, 0, 0]);
+
+  const updateSets = (reps: number, id: number): void => {
+    const setsCopy = [ ...sets ];
+    setsCopy[id] = reps;
+    setSets(setsCopy);
+  }
+
   return (
-    <div>Hello world!</div>
+    <main>
+      <form>
+        <div>
+          <label htmlFor="set-1-slider">Set 1</label>
+          <input type="range" id="set-1-slider" name="set-1-slider" min="0" max="10" value={sets[0]} onChange={e => updateSets(Number.parseInt(e.target.value), 0)} />
+          {sets[0]} reps
+        </div>
+        <div>
+          <label htmlFor="set-2-slider">Set 2</label>
+          <input type="range" id="set-2-slider" name="set-2-slider" min="0" max="10" value={sets[1]} onChange={e => updateSets(Number.parseInt(e.target.value), 1)} />
+          {sets[1]} reps
+        </div>
+      </form>
+    </main>
   );
 }
 
