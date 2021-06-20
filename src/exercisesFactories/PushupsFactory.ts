87346aafedb4b2e6dd965f801792df0f3ba3b22d
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-06-20 10:00:47 +0200

    Refactor PushupsFactory

--- src/exercisesFactories/PushupsFactory.ts
+++ src/exercisesFactories/PushupsFactory.ts
@@ -1,202 +1,96 @@
-import { Level, Sets, Exercise } from '../Types';
+import { Level, Sets, Exercise, Unit } from '../Types';
 
 export const PushupsFactory = (level: Level): Exercise => {
   switch (level) {
     case 1:
-      return WallPushups(1, 1, 10);
+      return Pushups('Wall pushups', 1, 1, 10, 'Reps');
     case 2:
-      return WallPushups(2, 2, 25);
+      return Pushups('Wall pushups', 2, 2, 25, 'Reps');
     case 3:
-      return WallPushups(3, 3, 50);
+      return Pushups('Wall pushups', 3, 3, 50, 'Reps');
 
     case 4:
-      return InclinePushups(4, 1, 10);
+      return Pushups('Incline pushups', 4, 1, 10, 'Reps');
     case 5:
-      return InclinePushups(5, 2, 20);
+      return Pushups('Incline pushups', 5, 2, 20, 'Reps');
     case 6:
-      return InclinePushups(6, 3, 40);
+      return Pushups('Incline pushups', 6, 3, 40, 'Reps');
 
     case 7:
-      return KneelingPushups(7, 1, 10);
+      return Pushups('Kneeling pushups', 7, 1, 10, 'Reps');
     case 8:
-      return KneelingPushups(8, 2, 15);
+      return Pushups('Kneeling pushups', 8, 2, 15, 'Reps');
     case 9:
-      return KneelingPushups(9, 3, 30);
+      return Pushups('Kneeling pushups', 9, 3, 30, 'Reps');
 
     case 10:
-      return HalfPushups(10, 1, 8);
+      return Pushups('Half pushups', 10, 1, 8, 'Reps');
     case 11:
-      return HalfPushups(11, 2, 12);
+      return Pushups('Half pushups', 11, 2, 12, 'Reps');
     case 12:
-      return HalfPushups(12, 2, 25);
+      return Pushups('Half pushups', 12, 2, 25, 'Reps');
 
     case 13:
-      return FullPushups(13, 1, 5);
+      return Pushups('Full pushups', 13, 1, 5, 'Reps');
     case 14:
-      return FullPushups(14, 2, 10);
+      return Pushups('Full pushups', 14, 2, 10, 'Reps');
     case 15:
-      return FullPushups(15, 2, 20);
+      return Pushups('Full pushups', 15, 2, 20, 'Reps');
 
     case 16:
-      return ClosePushups(16, 1, 5);
+      return Pushups('Close pushups', 16, 1, 5, 'Reps');
     case 17:
-      return ClosePushups(17, 2, 10);
+      return Pushups('Close pushups', 17, 2, 10, 'Reps');
     case 18:
-      return ClosePushups(18, 2, 20);
+      return Pushups('Close pushups', 18, 2, 20, 'Reps');
 
     case 19:
-      return UnevenPushups(19, 1, 5);
+      return Pushups('Uneven pushups', 19, 1, 5, 'Reps');
     case 20:
-      return UnevenPushups(20, 2, 10);
+      return Pushups('Uneven pushups', 20, 2, 10, 'Reps');
     case 21:
-      return UnevenPushups(21, 2, 20);
+      return Pushups('Uneven pushups', 21, 2, 20, 'Reps');
 
     case 22:
-      return HalfOneArmPushups(22, 1, 5);
+      return Pushups('1/2 one-arm pushups', 22, 1, 5, 'Reps');
     case 23:
-      return HalfOneArmPushups(23, 2, 10);
+      return Pushups('1/2 one-arm pushups', 23, 2, 10, 'Reps');
     case 24:
-      return HalfOneArmPushups(24, 2, 20);
+      return Pushups('1/2 one-arm pushups', 24, 2, 20, 'Reps');
 
     case 25:
-      return LeverPushups(25, 1, 5);
+      return Pushups('Lever pushups', 25, 1, 5, 'Reps');
     case 26:
-      return LeverPushups(26, 2, 10);
+      return Pushups('Lever pushups', 26, 2, 10, 'Reps');
     case 27:
-      return LeverPushups(27, 2, 20);
+      return Pushups('Lever pushups', 27, 2, 20, 'Reps');
 
     case 28:
-      return OneArmPushups(28, 1, 5);
+      return Pushups('One-arm pushups', 28, 1, 5, 'Reps');
     case 29:
-      return OneArmPushups(29, 2, 10);
+      return Pushups('One-arm pushups', 29, 2, 10, 'Reps');
     case 30:
-      return OneArmPushups(30, 2, 20);
+      return Pushups('One-arm pushups', 30, 2, 20, 'Reps');
 
     default:
-      return WallPushups(1, 1, 10);
+      return Pushups('Wall pushups', 1, 1, 10, 'Reps');
   }
 };
 
-const WallPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Wall pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const InclinePushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Incline pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const KneelingPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Kneeling pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const HalfPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Half pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const FullPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Full pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const ClosePushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Close pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const UnevenPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Uneven pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const HalfOneArmPushups = (
+const Pushups = (
+  variant: string,
   level: Level,
   sets: Sets,
-  reps: number
+  reps: number,
+  unit: Unit
 ): Exercise => {
   return {
     group: 'Pushups',
-    variant: 'Half one arm pushups',
+    variant,
     level,
     sets,
     reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const LeverPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'Lever pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/pushups/${level}.jpg`,
-  };
-};
-
-const OneArmPushups = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Pushups',
-    variant: 'One arm pushups',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
+    unit,
     image: `./images/pushups/${level}.jpg`,
   };
 };
