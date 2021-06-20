eb2fb4bc612ab28a1f2ebc56ff85ba5e55eaaf20
Johan Lindkvist <johanlindkvist89@gmail.com>
2021-06-20 10:09:40 +0200

    Refactor LegRaisesFactory

--- src/exercisesFactories/LegRaisesFactory.ts
+++ src/exercisesFactories/LegRaisesFactory.ts
@@ -1,226 +1,96 @@
-import { Level, Sets, Exercise } from '../Types';
+import { Level, Sets, Exercise, Unit } from '../Types';
 
 export const LegRaisesFactory = (level: Level): Exercise => {
   switch (level) {
     case 1:
-      return KneeTucks(1, 1, 10);
+      return LegRaises('Knee tucks', 1, 1, 10, 'Reps');
     case 2:
-      return KneeTucks(2, 2, 25);
+      return LegRaises('Knee tucks', 2, 2, 25, 'Reps');
     case 3:
-      return KneeTucks(3, 3, 40);
+      return LegRaises('Knee tucks', 3, 3, 40, 'Reps');
 
     case 4:
-      return FlatKneeRaises(4, 1, 10);
+      return LegRaises('Flat knee raises', 4, 1, 10, 'Reps');
     case 5:
-      return FlatKneeRaises(5, 2, 20);
+      return LegRaises('Flat knee raises', 5, 2, 20, 'Reps');
     case 6:
-      return FlatKneeRaises(6, 3, 35);
+      return LegRaises('Flat knee raises', 6, 3, 35, 'Reps');
 
     case 7:
-      return FlatBentLegRaises(7, 1, 10);
+      return LegRaises('Flat bent leg raises', 7, 1, 10, 'Reps');
     case 8:
-      return FlatBentLegRaises(8, 2, 15);
+      return LegRaises('Flat bent leg raises', 8, 2, 15, 'Reps');
     case 9:
-      return FlatBentLegRaises(9, 3, 30);
+      return LegRaises('Flat bent leg raises', 9, 3, 30, 'Reps');
 
     case 10:
-      return FlatFrogRaises(10, 1, 8);
+      return LegRaises('Flat frog raises', 10, 1, 8, 'Reps');
     case 11:
-      return FlatFrogRaises(11, 2, 15);
+      return LegRaises('Flat frog raises', 11, 2, 15, 'Reps');
     case 12:
-      return FlatFrogRaises(12, 2, 25);
+      return LegRaises('Flat frog raises', 12, 2, 25, 'Reps');
 
     case 13:
-      return FlatStraightLegRaises(13, 1, 5);
+      return LegRaises('Flat straight leg raises', 13, 1, 5, 'Reps');
     case 14:
-      return FlatStraightLegRaises(14, 2, 10);
+      return LegRaises('Flat straight leg raises', 14, 2, 10, 'Reps');
     case 15:
-      return FlatStraightLegRaises(15, 2, 20);
+      return LegRaises('Flat straight leg raises', 15, 2, 20, 'Reps');
 
     case 16:
-      return HangingKneeRaises(16, 1, 5);
+      return LegRaises('Hanging knee raises', 16, 1, 5, 'Reps');
     case 17:
-      return HangingKneeRaises(17, 2, 10);
+      return LegRaises('Hanging knee raises', 17, 2, 10, 'Reps');
     case 18:
-      return HangingKneeRaises(18, 2, 15);
+      return LegRaises('Hanging knee raises', 18, 2, 15, 'Reps');
 
     case 19:
-      return HangingBentLegRaises(19, 1, 5);
+      return LegRaises('Hanging bent leg raises', 19, 1, 5, 'Reps');
     case 20:
-      return HangingBentLegRaises(20, 2, 10);
+      return LegRaises('Hanging bent leg raises', 20, 2, 10, 'Reps');
     case 21:
-      return HangingBentLegRaises(21, 2, 15);
+      return LegRaises('Hanging bent leg raises', 21, 2, 15, 'Reps');
 
     case 22:
-      return HangingFrogRaises(22, 1, 5);
+      return LegRaises('Hanging frog raises', 22, 1, 5, 'Reps');
     case 23:
-      return HangingFrogRaises(23, 2, 10);
+      return LegRaises('Hanging frog raises', 23, 2, 10, 'Reps');
     case 24:
-      return HangingFrogRaises(24, 2, 15);
+      return LegRaises('Hanging frog raises', 24, 2, 15, 'Reps');
 
     case 25:
-      return PartialStraightLegRaises(25, 1, 5);
+      return LegRaises('Partial straight leg raises', 25, 1, 5, 'Reps');
     case 26:
-      return PartialStraightLegRaises(26, 2, 10);
+      return LegRaises('Partial straight leg raises', 26, 2, 10, 'Reps');
     case 27:
-      return PartialStraightLegRaises(27, 2, 15);
+      return LegRaises('Partial straight leg raises', 27, 2, 15, 'Reps');
 
     case 28:
-      return HangingStraightLegRaises(28, 1, 5);
+      return LegRaises('Hanging straight leg raises', 28, 1, 5, 'Reps');
     case 29:
-      return HangingStraightLegRaises(29, 2, 10);
+      return LegRaises('Hanging straight leg raises', 29, 2, 10, 'Reps');
     case 30:
-      return HangingStraightLegRaises(30, 2, 30);
+      return LegRaises('Hanging straight leg raises', 30, 2, 30, 'Reps');
 
     default:
-      return KneeTucks(1, 1, 10);
+      return LegRaises('Knee tucks', 1, 1, 10, 'Reps');
   }
 };
 
-const KneeTucks = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Knee tucks',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const FlatKneeRaises = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Flat knee raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const FlatBentLegRaises = (
+const LegRaises = (
+  variant: string,
   level: Level,
   sets: Sets,
-  reps: number
+  reps: number,
+  unit: Unit
 ): Exercise => {
   return {
     group: 'Leg raises',
-    variant: 'Flat bent leg raises',
+    variant,
     level,
     sets,
     reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const FlatFrogRaises = (level: Level, sets: Sets, reps: number): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Flat frog raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const FlatStraightLegRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Flat straight leg raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const HangingKneeRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Hanging knee raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const HangingBentLegRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Hanging bent leg raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const HangingFrogRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Hanging frog raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const PartialStraightLegRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Partial straight leg raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
-    image: `./images/legRaises/${level}.jpg`,
-  };
-};
-
-const HangingStraightLegRaises = (
-  level: Level,
-  sets: Sets,
-  reps: number
-): Exercise => {
-  return {
-    group: 'Leg raises',
-    variant: 'Hanging straight leg raises',
-    level,
-    sets,
-    reps,
-    unit: 'Reps',
+    unit,
     image: `./images/legRaises/${level}.jpg`,
   };
 };
